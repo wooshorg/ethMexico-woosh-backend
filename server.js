7aac3873820431b46c894e51b6e84d251bbc26c9
Kenk <kenghin_lim@hotmail.com>
2022-08-21 07:21:46 +0800

    update server.js

--- server.js
+++ server.js
@@ -59,11 +59,7 @@ const user = require("./routes/User");
 // ----------------------------------
 const app = express();
 
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+app.use(cors());
 app.use(helmet());
 
 app.use(express.json());
