69a929b3830bca689b9b33f9bc7dcd67207e7710
Kenk <kenghin_lim@hotmail.com>
2022-08-21 18:32:07 +0800

    ASD

--- models/User.model.js
+++ models/User.model.js
@@ -3,23 +3,15 @@ const mongoose = require("mongoose");
 const User = new mongoose.Schema({
   address: {
     type: String,
-    required: true,
-    unique: true,
   },
   worldcoin_hash: {
     type: String,
-    required: true,
-    unique: true,
   },
   username: {
     type: String,
-    required: true,
-    unique: true,
   },
   image_url: {
     type: String,
-    required: true,
-    unique: true,
   },
 });
 
